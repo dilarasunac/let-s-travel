302e367b14eba590b87b23fec627df8f3188b05d
Dilara <dlrsnc2@gmail.com>
2026-07-23 21:14:14 +0300

    Web ana ekran ikonu ve manifest ekle

---
+++ app/+html.tsx
@@ -0,0 +1,21 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import { type PropsWithChildren } from 'react';
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="tr">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <ScrollViewStyleReset />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Let's Travel" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
